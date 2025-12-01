844711340ff9c1a87b45281e370ccd74f6672c22
Asritha Kuruguntla <asritha.kuruguntla@gmail.com>
2025-12-01 18:15:38 +0530

    changes made and report added

--- ticket-service/src/main/java/com/example/feign/FlightInterface.java
+++ ticket-service/src/main/java/com/example/feign/FlightInterface.java
@@ -5,11 +5,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import com.example.exception.ResourceNotFoundException;
 import com.example.response.FlightResponse;
 
 @FeignClient(name = "FLIGHT-SERVICE")
 
 public interface FlightInterface {
 	@GetMapping("/flight/getFlightById/{id}")
-	public ResponseEntity<FlightResponse> getByID(@PathVariable int id);
+	public ResponseEntity<FlightResponse> getByID(@PathVariable int id) throws ResourceNotFoundException;
 }
